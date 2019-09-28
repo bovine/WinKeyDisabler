9a7afbdd71506e256589a899915b8879705607d6
Jeff Lawson <jeff@bovine.net>
2019-09-27 21:04:48 -0500

    Add project files.

---
+++ StartButtonDisabler.cpp
@@ -0,0 +1,262 @@
+// StartButtonDisabler.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "StartButtonDisabler.h"
+
+#define MAX_LOADSTRING 100
+#define IDT_TIMER1 1
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HHOOK hook_keys;
+BOOL bIsCurrentlyFullScreen = FALSE;
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // Prevent multiple instances of ourself from running
+	HANDLE hMutex = CreateMutex(NULL, FALSE, _T("StartButtonDisablerMutex"));
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+		return 0;
+	}
+
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_STARTBUTTONDISABLER, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+	nCmdShow = SW_MINIMIZE;						// always start minimized
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_STARTBUTTONDISABLER));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_STARTBUTTONDISABLER));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_STARTBUTTONDISABLER);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX,
+      CW_USEDEFAULT, 0, 250, 250, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+
+
+// Implementation from https://stackoverflow.com/a/55542400
+bool isFullscreen(HWND windowHandle)
+{
+	MONITORINFO monitorInfo = { 0 };
+	monitorInfo.cbSize = sizeof(MONITORINFO);
+	GetMonitorInfo(MonitorFromWindow(windowHandle, MONITOR_DEFAULTTOPRIMARY), &monitorInfo);
+
+	RECT windowRect;
+	GetWindowRect(windowHandle, &windowRect);
+
+	return windowRect.left == monitorInfo.rcMonitor.left
+		&& windowRect.right == monitorInfo.rcMonitor.right
+		&& windowRect.top == monitorInfo.rcMonitor.top
+		&& windowRect.bottom == monitorInfo.rcMonitor.bottom;
+}
+
+
+
+// Implementation from http://www.cplusplus.com/forum/general/217945/#msg1006712
+LRESULT CALLBACK LLKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	PKBDLLHOOKSTRUCT hookstruct;
+
+	if (nCode == HC_ACTION)
+	{
+		switch (wParam)
+		{
+		case WM_KEYDOWN:
+		case WM_SYSKEYDOWN:
+		case WM_KEYUP:
+		case WM_SYSKEYUP:
+
+			hookstruct = (PKBDLLHOOKSTRUCT)lParam;
+
+			if (bIsCurrentlyFullScreen && hookstruct->vkCode == 91) { /* pesky Windows button */
+				return 1;
+			} else {
+				return CallNextHookEx(NULL, nCode, wParam, lParam);
+			}
+		}
+	}
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+
+
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_CREATE: {
+		hook_keys = SetWindowsHookEx(WH_KEYBOARD_LL, LLKeyboardProc, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		SetTimer(hWnd, IDT_TIMER1, 1000, NULL);
+		return 0;
+	}
+	case WM_COMMAND: {
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	}
+	case WM_TIMER: {
+		// Every time our timer is called, check the current state of full screen apps.
+		BOOL newstate = isFullscreen(GetForegroundWindow());
+		if (bIsCurrentlyFullScreen != newstate) {
+			InvalidateRect(hWnd, NULL, TRUE);
+			bIsCurrentlyFullScreen = newstate;
+		}
+
+#ifdef _DEBUG
+		// Log a debug message
+		char msgBuffer[100];
+		_snprintf_s(msgBuffer, sizeof(msgBuffer), "Full screen mode is now %d\n", (int)bIsCurrentlyFullScreen);
+		OutputDebugStringA(msgBuffer);
+#endif
+		break;
+	}
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // Paint the client area black if in full-screen mode,  or white if in normal mode.
+		RECT windowRect;
+		GetClientRect(hWnd, &windowRect);
+		HBRUSH hbr = (HBRUSH) (bIsCurrentlyFullScreen ? GetStockObject(BLACK_BRUSH) : GetStockObject(WHITE_BRUSH));
+		FillRect(hdc, &windowRect, hbr);
+        EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY: {
+		UnhookWindowsHookEx(hook_keys);
+		PostQuitMessage(0);
+		break;
+	}
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
